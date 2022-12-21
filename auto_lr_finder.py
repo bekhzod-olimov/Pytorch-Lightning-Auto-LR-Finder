b968b605931f9097d9239cf041d76b99fd46e625
Olimov Bekhzod <50166164+bekhzod-olimov@users.noreply.github.com>
2022-12-21 16:24:19 +0900

    Update auto_lr_finder.py

--- auto_lr_finder.py
+++ auto_lr_finder.py
@@ -1,4 +1,4 @@
-import os, argparse, yaml, torch, torchvision, timm, pickle, wandb
+import os, argparse, yaml, torch, torchvision, timm, pickle, wandb, AutoAugment
 from datetime import datetime
 import pytorch_lightning as pl
 from torch.nn import *
@@ -10,8 +10,10 @@ from torch.optim.lr_scheduler import *
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.loggers import WandbLogger
 from collections import OrderedDict as OD
+from sketch_dataset import SketchyImageDataset
 from collections import namedtuple as NT
 from softdataset import TripletImageDataset, data_split
+from original_dataset import OriginalImageDataset, data_split
 from tqdm import tqdm
 import torchvision.transforms.functional as TF
 
@@ -38,54 +40,54 @@ def run(args):
     model_dict[model_name] = 0 
     os.system('wandb login 3204eaa1400fed115e40f43c7c6a5d62a0867ed1')
     
-    angles=[-30, -15, 0, 15, 30] # rotation 각도, 이중 하나의 각도로 돌아감 [-30, -15, 0, 15, 30]
-    distortion_scale = 0.5 # distortion 각도 (0.5)
-    p = 0.5 # distortion이 적용될 확률 (0.5)
-    fill = [255,255,255] # distortion 후 채워지는 배경색
-
     transformations = {}   
 
     transformations['qry'] = transforms.Compose([
-                            transforms.RandomHorizontalFlip(),
-                            transforms.RandomAffine(0, (0.5, 0), fill = fill),
-                            transforms.RandomRotation(90, fill = fill),
-                            transforms.ColorJitter([0.3, 1]),
-                            transforms.GaussianBlur(9, (0.5, 3.0)),
+                            transforms.Resize((224,224)),
+                            AutoAugment.ImageNetPolicy(),
+                            # transforms.RandomHorizontalFlip(),
+                            # transforms.RandomRotation(90, fill = fill),
+                            # transforms.ColorJitter([0.3, 1]),
+                            # transforms.GaussianBlur(9, (0.5, 3.0)),
                             transforms.ToTensor(),
                                                   ])
 
     transformations['pos'] = transforms.Compose([
-        transforms.RandomRotation(90, fill = fill),
-        transforms.RandomPerspective(distortion_scale = distortion_scale, p = p, fill = fill),
+        transforms.Resize((224,224)),
+        AutoAugment.ImageNetPolicy(),
+        # transforms.RandomRotation(90, fill = fill_sketch),
+        # transforms.RandomPerspective(distortion_scale = distortion_scale, p = p, fill = fill_sketch),
         transforms.ToTensor(),
     ])  
     transformations['neg'] = transforms.Compose([
-        transforms.RandomRotation(90, fill = fill),
-        transforms.RandomPerspective(distortion_scale = distortion_scale, p = p, fill = fill),
+        transforms.Resize((224,224)),
+        AutoAugment.ImageNetPolicy(),
+        # transforms.RandomRotation(90, fill = fill_sketch),
+        # transforms.RandomPerspective(distortion_scale = distortion_scale, p = p, fill = fill_sketch),
         transforms.ToTensor(),
     ])
-      
     
-    out_path = f"/home/ubuntu/workspace/bekhzod/triplet-loss-pytorch/pytorch_lightning/data/test_lr_dataset.json"
-       
-    tr_ds = TripletImageDataset(data_dir = path, transform_dic=transformations, random=True, trainval_json=out_path, trainval='train', load_images=True)
-    val_ds = TripletImageDataset(data_dir = path, transform_dic=transformations, random=True, trainval_json=out_path, trainval='val', load_images=True)
-    test_ds = TripletImageDataset(data_dir = path, transform_dic=transformations, random=True, trainval_json=out_path, trainval='test', load_images=True)
+    out_path = "data/sketchy_database_256_soft_split_cat.json"
+    # out_path = data_split(path, out_path)
+
+    tr_ds = SketchyImageDataset(data_dir = path, transform_dic=transformations, random=True, trainval_json=out_path, trainval='train', load_images=False)
+    val_ds = SketchyImageDataset(data_dir = path, transform_dic=transformations, random=True, trainval_json=out_path, trainval='val', load_images=False)
+    test_ds = SketchyImageDataset(data_dir = path, transform_dic=transformations, random=True, trainval_json=out_path, trainval='test', load_images=False)
     
-    milestones = [i*5 for i in range(1, 15)]
-    gamma = 0.01
-    wandb_logger = WandbLogger(name=f'lr={lr}-gamma={gamma}-{milestones}',project='Test LR')
-    num_classes = tr_ds.get_prod_length()
-    print(f"The dataset has {num_classes} classes")
-    
+    wandb_logger = WandbLogger(name=f'{model_name}_{datetime.now().strftime("%m/%d/%H:%M:%S")}_triplet_training', project='Train-test-LR')
+    num_classes = tr_ds.get_cat_length()
     print(f"Number of train set images: {len(tr_ds)}")
     print(f"Number of validation set images: {len(val_ds)}")
     print(f"Number of test set images: {len(test_ds)}")
+    print(f"\nTrain dataset has {num_classes} classes")
+    # print(f"Validation dataset has {num_classes} classes")
+    print(f"Validation dataset has {val_ds.get_cat_length()} classes")
+    print(f"Test dataset has {test_ds.get_cat_length()} classes")
     
     cos = CosineSimilarity(dim=1, eps=1e-6)
-    train_loader = DataLoader(tr_ds, batch_size=bs, shuffle=True, drop_last=True, num_workers=8)
+    train_loader = DataLoader(tr_ds, batch_size=bs, shuffle=True, drop_last=False, num_workers=8)
     val_loader = DataLoader(val_ds, batch_size=bs, shuffle=True, drop_last=True, num_workers=8)
-    test_loader = DataLoader(test_ds, batch_size=bs, shuffle=True, drop_last=True, num_workers=8)  
+    test_loader = DataLoader(test_ds, batch_size=bs, shuffle=True, drop_last=False, num_workers=8)  
     labels = {"pos": torch.tensor(1.).unsqueeze(0),
               "neg": torch.tensor(-1.).unsqueeze(0)}
     
@@ -145,7 +147,7 @@ def run(args):
             dic = {}                        
             fm = self.model.forward_features(inp)
             pool = AvgPool2d((fm.shape[2],fm.shape[3]))
-            lbl = self.model.forward_head(fm)
+            lbl = self.model.head(fm)
             dic["feature_map"] = torch.reshape(pool(fm), (-1, fm.shape[1]))
             dic["class_pred"] = lbl
             out = dict_to_namedtuple(dic)
@@ -162,6 +164,8 @@ def run(args):
             else:
                 assert False, f'Unknown optimizer: "{self.hparams.optimizer_name}"'
             
+            milestones = [5,10,15,20,25,30,35,40,45,50]
+            gamma=0.1
             scheduler = MultiStepLR(optimizer=optimizer, milestones=milestones, gamma=gamma, verbose=True)
         
             return [optimizer], [scheduler]
@@ -183,7 +187,8 @@ def run(args):
             # Compute loss
             if only_features and only_labels:
                 loss_cos = self.cos_loss(fm_ims, fm_poss, labels["pos"]) + self.cos_loss(fm_ims, fm_negs, labels["neg"])
-                loss_ce = self.ce_loss(lbl_ims, regs) + self.ce_loss(lbl_poss, regs)
+                # loss_ce = self.ce_loss(lbl_ims, regs) + self.ce_loss(lbl_poss, regs)
+                loss_ce = self.ce_loss(lbl_ims, clss) + self.ce_loss(lbl_poss, clss)
                 loss = loss_cos + loss_ce 
             elif only_features == True and only_labels == None:
                 loss_cos = self.cos_loss(fm_ims, fm_poss, labels["pos"]) + self.cos_loss(fm_ims, fm_negs, labels["neg"])
@@ -203,7 +208,7 @@ def run(args):
                 if regs[idx] in inds[0]:
                     top1 += 1
 
-            return OD([('loss', loss)]) #, ('train_top3', top3 / len(ims)), ('train_top1', top1 / len(ims))])  # Return tensor to call ".backward" on
+            return OD([('loss', loss)]) 
 
         def validation_step(self, batch, batch_idx): # triplet loss 
 
@@ -221,7 +226,8 @@ def run(args):
             # Compute loss
             if only_features and only_labels:                
                 loss_cos = self.cos_loss(fm_ims, fm_poss, labels["pos"]) + self.cos_loss(fm_ims, fm_negs, labels["neg"])
-                loss_ce = self.ce_loss(lbl_ims, regs) + self.ce_loss(lbl_poss, regs)
+                # loss_ce = self.ce_loss(lbl_ims, regs) + self.ce_loss(lbl_poss, regs)
+                loss_ce = self.ce_loss(lbl_ims, clss) + self.ce_loss(lbl_poss, clss)
                 print(f"Triplet loss: {loss_cos:.3f}")
                 print(f"Crossentropy loss: {loss_ce:.3f}")
                 loss = loss_cos + loss_ce 
@@ -254,77 +260,6 @@ def run(args):
 
             return OD([('loss', loss), ('val_top3', top3),
                        ('cos_sims', torch.mean(torch.FloatTensor(cos_sims)))])
-
-        def test_step(self, batch, batch_idx): # triplet loss 
-            
-            """ 
-            Compares test images in the batch with the all images in the dataloader.
-            
-            """
-            fms_ims, fms_poss, fms_negs, scores, gts_all, im_pred_lbls_all, pos_pred_lbls_all = [], [], [], [], [], [], []
-            top1, top3, = 0, 0
-            
-            print("\nObtaining embeddings and predicting labels...\n")
-            # Get feature maps and pred labels of the whole test data
-            for i, batch_all in enumerate(test_loader):
-                with torch.no_grad():
-                    
-                    ims_all, poss_all, negs_all, clss_all, regs_all = batch_all['qry'], batch_all['pos'][0], batch_all['neg'][0], batch_all['cat_idx'], batch_all['prod_idx']
-                    out_ims_all = self(ims_all.cuda())
-                    fm_ims_all, lbl_ims_all = out_ims_all[0], out_ims_all[1] # get feature maps [0] and predicted labels [1]
-                    out_poss_all = self(poss_all.cuda())
-                    fm_poss_all, lbl_poss_all = out_poss_all[0], out_poss_all[1] # get feature maps [0] and predicted labels [1]
-                    out_negs_all = self(negs_all.cuda())
-                    fm_negs_all, lbl_negs_all = out_negs_all[0], out_negs_all[1] # get feature maps [0] and predicted labels [1]
-                    fms_ims.extend(fm_ims_all)
-                    fms_poss.extend(fm_poss_all)
-                    fms_negs.extend(fm_negs_all)
-                    gts_all.extend(regs_all.cuda())
-                    im_pred_lbls_all.extend(lbl_ims_all)
-                    pos_pred_lbls_all.extend(lbl_poss_all)
-                
-            fms_ims = torch.stack(fms_ims)
-            fms_poss = torch.stack(fms_poss)
-            fms_negs = torch.stack(fms_negs)
-            gts_all = torch.stack(gts_all)
-            im_pred_lbls_all = torch.stack(im_pred_lbls_all)
-            pos_pred_lbls_all = torch.stack(pos_pred_lbls_all)
-            
-            print("Done!\n")               
-            print("\nCalculating metrics...")
-        
-            # Compute loss, top3, and top1   
-            for index, fm in enumerate(fms_ims):
-                
-                # Compute loss
-                if only_features and only_labels:
-                    loss_cos = self.cos_loss(fm.unsqueeze(0), fms_poss[index].unsqueeze(0), labels["pos"].to("cuda")) + self.cos_loss(fm.unsqueeze(0), fms_negs[index].unsqueeze(0), labels["neg"].to("cuda"))
-                    loss_ce = self.ce_loss(im_pred_lbls_all[index], gts_all[index]) + self.ce_loss(pos_pred_lbls_all[index], gts_all[index])
-                    loss = loss_cos + loss_ce 
-                elif only_features == True and only_labels == None:
-                    loss_cos = self.cos_loss(fm.unsqueeze(0), fms_poss[index].unsqueeze(0), labels["pos"].to("cuda")) + self.cos_loss(fm.unsqueeze(0), fms_negs[index].unsqueeze(0), labels["neg"].to("cuda"))
-                    loss = loss_cos                 
-                elif only_features == None and only_labels == True:
-                    loss_ce = self.ce_loss(im_pred_lbls_all[index], gts_all[index]) + self.ce_loss(pos_pred_lbls_all[index], gts_all[index])
-                    loss = loss_ce 
-                
-                score = cos(fm.unsqueeze(0), fms_poss[index].unsqueeze(0)) # (1, fm), (len(dl), fm) = (len(dl), fm)
-                scores.append(score)
-                vals, inds = torch.topk(im_pred_lbls_all[index], k=3)
-                if gts_all[index] in inds:
-                    top3 += 1
-                if gts_all[index] in inds[0]:
-                    top1 += 1                    
-
-            print("Calculating metrics is done!\n")
-            print('top1', top1/len(fms_ims))
-            print('top3', top3/len(fms_ims))
-            print('test_loss', loss / len(fms_ims))
-
-            self.log("test_sim_scores", cos_sim_score(torch.mean(torch.FloatTensor(scores)).item(), eps, alpha, mode='for_pos'))
-            self.log("test_loss", loss / len(fms_ims))
-            self.log("test_top3", top3 / len(fms_ims))
-            self.log("test_top1", top1 / len(fms_ims))
 
     def create_model(model_name, conv_input=False, num_classes=num_classes):
         
@@ -425,7 +360,7 @@ if __name__ == "__main__":
     parser.add_argument("-sp", "--save_path", type=str, default='saved_models', help="Path to save trained models")
     parser.add_argument("-bs", "--batch_size", type=int, default=64, help="Batch size")
     parser.add_argument("-d", "--device", type=str, default='cuda:1', help="GPU device number")
-    parser.add_argument("-ip", "--ims_path", type=str, default='/home/ubuntu/workspace/dataset/test_dataset_svg/test_dataset_svg_1122_3_org', help="Path to the images")
+    parser.add_argument("-ip", "--ims_path", type=str, default='/home/ubuntu/workspace/dataset/sketchy_database_256', help="Path to the images")
     parser.add_argument("-is", "--input_size", type=int, default=(224, 224), help="Size of the images")
     parser.add_argument("-mn", "--model_name", type=str, default='rexnet_150', help="Model name (from timm library (ex. darknet53, ig_resnext101_32x32d))")
     parser.add_argument("-on", "--optimizer_name", type=str, default='Adam', help="Optimizer name (Adam or SGD)")
